bd3d986878a15d7f4a09ebf4965f6818091fba27
namad <88336308+namad12@users.noreply.github.com>
2024-10-16 19:33:13 +0700

    Update Ch_7.sql

--- 2_SqlOracle/Ch_7.sql
+++ 2_SqlOracle/Ch_7.sql
@@ -5,14 +5,22 @@ SELECT last_name,
        CEIL(MONTHS_BETWEEN(SYSDATE, hire_date)) AS MONTHS_WORKED
 FROM employee
 ORDER BY MONTHS_WORKED DESC;
+--> OK
 
 --b
 SELECT last_name || ' earns ' || salary || ' monthly but wants ' || (salary * 3) AS "Dream Salaries"
 FROM employee;
+--> OK
 
 --c
 SELECT last_name, TO_CHAR(salary, '$999,999,999') AS "SALARY"
 FROM employee;
+
+--> 
+SELECT last_name,
+       LPAD('$' || TO_CHAR(salary, 'FM999999999999999'), 15) AS SALARY
+FROM employees;
+
 
 --d
 SELECT last_name,
@@ -22,3 +30,6 @@ SELECT last_name,
            'Day, "the" day "of" Month, YYYY'
        ) AS REVIEW
 FROM employee;
+--> OK
+
+--> còn câu E, F, G, H, I, J, K đâu rồi nhể
