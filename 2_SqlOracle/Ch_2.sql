4afb6536eb303f27b548bf9bda6dc3063bc53984
namad <88336308+namad12@users.noreply.github.com>
2024-10-16 23:24:23 +0700

    Review Ch_2 Done.sql

--- 2_SqlOracle/Ch_2.sql
+++ 2_SqlOracle/Ch_2.sql
@@ -1,61 +1,84 @@
-
---B�i chuong 2
--a
-CREATE TABLE SYS.Employee (
-    Employee_id NUMBER(6,0) NOT NULL,
-    First_name  VARCHAR(20),
-    Last_name   VARCHAR(25) NOT NULL,
-    Email   VARCHAR(20) NOT NULL,
+--Bài chuong 2
+--a
+CREATE TABLE MYSCHEMA.Employee (
+    Employee_id     NUMBER(6,0) NOT NULL,
+    First_name      VARCHAR(20),
+    Last_name       VARCHAR(25) NOT NULL,
+    Email           VARCHAR(20) NOT NULL,
     Phone_number    VARCHAR(20),
-    Hire_date   DATE NOT NULL,
-    Job_id  VARCHAR(10) NOT NULL,
-    Salary  NUMBER(8,2),
+    Hire_date       DATE        NOT NULL,
+    Job_id          VARCHAR(10) NOT NULL,
+    Salary          NUMBER(8,2),
     Commission_pct  NUMBER(2,2),
-    Manager_id  NUMBER(6,0),
+    Manager_id      NUMBER(6,0),
     Department_id   NUMBER(4,0)
 );
 
-SELECT * FROM  SYS.Employee;
+SELECT * FROM  MYSCHEMA.Employee;
 
-CREATE TABLE SYS.Department(
-    Department_id   NUMBER(4,0) NOT NULL,
-    Department_name VARCHAR2(30) NOT NULL,
+--
+CREATE TABLE MYSCHEMA.Department(
+    Department_id   NUMBER(4,0)     NOT NULL,
+    Department_name VARCHAR2(30)    NOT NULL,
     Manager_id  NUMBER(6,0),
     Location_id NUMBER(4,0)
 );
 
-SELECT * FROM  SYS.department;
+SELECT * FROM  MYSCHEMA.department;
 
-CREATE TABLE SYS.Job_Grades(
-    Grade   CHAR(1) NOT NULL,
+--
+CREATE TABLE MYSCHEMA.Job_Grades(
+    Grade       CHAR(1)     NOT NULL,
     Lowest_sal  NUMBER(8,2) NOT NULL,
     Highest_sal NUMBER(8,2) NOT NULL
 );
-SELECT * FROM SYS.job_grades;
+
+SELECT * FROM MYSCHEMA.job_grades;
+
+--> không dùng schema sys
+--> còn lại thì OK
 
 --b
-CREATE TABLE SYS.Dept(
+/*CREATE TABLE MYSCHEMA.Dept(
     Dept_id NUMBER(7,0) CONSTRAINT de_de_id_pk PRIMARY KEY,
     Depy_name   VARCHAR(25)  
 );
 
-SELECT * FROM SYS.Dept;
+SELECT * FROM MYSCHEMA.Dept;*/
+
+--> Tạo sai tên cột, xem lại đề bài
+--> de_de_id_pk => sao lại để tên primary key như này
+--> Sửa code thành như này
+CREATE TABLE MYSCHEMA.Dept(
+    ID      NUMBER(7,0) ,
+    NAME    VARCHAR2(25)
+);
+ALTER TABLE MYSCHEMA.Dept
+ADD CONSTRAINT pk_dept_id PRIMARY KEY (id);
+
+SELECT * FROM MYSCHEMA.Dept;
 
 --c
-CREATE TABLE SYS.Emp(
+CREATE TABLE MYSCHEMA.Emp(
     Emp_id NUMBER(7,0),
     Last_name   VARCHAR2(25),
     First_name  VARCHAR2(25),
     Dept_id NUMBER(7,0),
-    CONSTRAINT emp_de_id_fk FOREIGN KEY (Dept_id) REFERENCES SYS.dept(Dept_id)
+    CONSTRAINT emp_de_id_fk FOREIGN KEY (Dept_id) REFERENCES MYSCHEMA.dept(Dept_id)
 );
 
-SELECT * FROM SYS.Emp;
+SELECT * FROM MYSCHEMA.Emp;
+
+--> tương tự câu b, sai tên cột, xem lại column name trong đề bài
+--> Dept_id = ID
 
 --d
-ALTER TABLE SYS.emp
+ALTER TABLE MYSCHEMA.emp
 ADD Email VARCHAR2(25);
 
+--> ok
+
+--e
 CREATE TABLE Emp2 AS
 SELECT 
     EMPLOYEE_ID AS ID, 
@@ -63,37 +86,37 @@ SELECT
     LAST_NAME, 
     SALARY, 
     DEPARTMENT_ID AS DEPT_ID
-FROM SYS.Employee;
+FROM MYSCHEMA.Employee;
 
-SELECT * FROM SYS.Emp2;
+SELECT * FROM MYSCHEMA.Emp2;
 
---e
-DROP TABLE SYS.Emp;
+--> ok
+
+--f
+DROP TABLE MYSCHEMA.Emp; --> ok
 
 --g
-INSERT INTO SYS.employee VALUES (100, 'Steven', 'King', 'SKING', '515.123.4567', TO_DATE('17-06-1987', 'DD-MM-YYYY'), 'AD_PRES', 24000, NULL, NULL, 90);
-INSERT INTO SYS.Employee VALUES (101, 'Neena', 'Kochhar', 'NKOCHHAR', '515.123.4568', TO_DATE('21-09-1989', 'DD-MM-YYYY'), 'AD_VP', 17000, NULL, 100, 90);
-INSERT INTO SYS.Employee VALUES (102, 'Lex', 'De Haan', 'LDEHAAN', '515.123.4569', TO_DATE('13-01-1993', 'DD-MM-YYYY'), 'AD_VP', 17000, NULL, 100, 90);
-INSERT INTO SYS.Employee VALUES (103, 'Alexander', 'Hunold', 'AHUNOLD', '590.423.4567', TO_DATE('03-01-1990', 'DD-MM-YYYY'), 'IT_PROG', 9000, NULL, 102, 60);
-INSERT INTO SYS.Employee VALUES (104, 'Bruce', 'Ernst', 'BERNST', '590.423.4568', TO_DATE('21-05-1991', 'DD-MM-YYYY'), 'IT_PROG', 6000, NULL, 103, 60);
-INSERT INTO SYS.Employee VALUES (107, 'Diana', 'Lorentz', 'DLORENTZ', '590.423.5567', TO_DATE('07-02-1999', 'DD-MM-YYYY'), 'IT_PROG', 4200, NULL, 103, 60);
-INSERT INTO SYS.Employee VALUES (124, 'Kevin', 'Mourgos', 'KMOURGOS', '650.123.5234', TO_DATE('16-11-1999', 'DD-MM-YYYY'), 'ST_MAN', 5800, NULL, 100, 50);
-INSERT INTO SYS.Employee VALUES (141, 'Trenna', 'Rajs', 'TRAJS', '650.121.8009', TO_DATE('17-10-1995', 'DD-MM-YYYY'), 'ST_CLERK', 3500, NULL, 124, 50);
-INSERT INTO SYS.Employee VALUES (142, 'Curtis', 'Davies', 'CDAVIES', '650.121.2994', TO_DATE('29-01-1997', 'DD-MM-YYYY'), 'ST_CLERK', 3100, NULL, 124, 50);
-INSERT INTO SYS.Employee VALUES (143, 'Randall', 'Matos', 'RMATOS', '650.121.2874', TO_DATE('15-03-1998', 'DD-MM-YYYY'), 'ST_CLERK', 2600, NULL, 124, 50);
-INSERT INTO SYS.Employee VALUES (144, 'Peter', 'Vargas', 'PVARGAS', '650.121.2004', TO_DATE('09-07-1998', 'DD-MM-YYYY'), 'ST_CLERK', 2500, NULL, 124, 50);
-INSERT INTO SYS.Employee VALUES (149, 'Eleni', 'Zlotkey', 'EZLOTKEY', '011.44.1344.429018', TO_DATE('29-01-2000', 'DD-MM-YYYY'), 'SA_MAN', 10500, 0.2, 100, 80);
-INSERT INTO SYS.Employee VALUES (174, 'Ellen', 'Abel', 'EABEL', '011.44.1644.429267', TO_DATE('11-05-1996', 'DD-MM-YYYY'), 'SA_REP', 11000, 0.3, 149, 80);
-INSERT INTO SYS.Employee VALUES (176, 'Jonathon', 'Taylor', 'JTAYLOR', '011.44.1644.429265', TO_DATE('24-03-1998', 'DD-MM-YYYY'), 'SA_REP', 8600, 0.2, 149, 80);
-INSERT INTO SYS.Employee VALUES (178, 'Kimberely', 'Grant', 'KGRANT', '011.44.1644.429263', TO_DATE('24-05-1999', 'DD-MM-YYYY'), 'SA_REP', 7000, 0.15, 149, NULL);
-INSERT INTO SYS.Employee VALUES (200, 'Jennifer', 'Whalen', 'JWHALEN', '515.123.4444', TO_DATE('17-09-1987', 'DD-MM-YYYY'), 'AD_ASST', 4400, NULL, 101, 10);
-INSERT INTO SYS.Employee VALUES (201, 'Michael', 'Hartstein', 'MHARTSTE', '515.123.5555', TO_DATE('17-02-1996', 'DD-MM-YYYY'), 'MK_MAN', 13000, NULL, 100, 20);
-INSERT INTO SYS.Employee VALUES (202, 'Pat', 'Fay', 'PFAY', '603.123.6666', TO_DATE('17-08-1997', 'DD-MM-YYYY'), 'MK_REP', 6000, NULL, 201, 20);
-INSERT INTO SYS.Employee VALUES (205, 'Shelley', 'Higgins', 'SHIGGINS', '515.123.8080', TO_DATE('07-06-1994', 'DD-MM-YYYY'), 'AC_MGR', 12000, NULL, 101, 110);
-INSERT INTO SYS.Employee VALUES (206, 'William', 'Gietz', 'WGIETZ', '515.123.8181', TO_DATE('07-06-1994', 'DD-MM-YYYY'), 'AC_ACCOUNT', 8300, NULL, 205, 110);
-INSERT INTO SYS.Employee VALUES (207, 'TEST', 'TEST', 'TEST', '084.123.4567', TO_DATE('17-06-2018', 'DD-MM-YYYY'), 'IT_PROG', 24000, NULL, NULL, 60);
-
-DELETE FROM employee
+INSERT INTO MYSCHEMA.employee VALUES (100, 'Steven', 'King', 'SKING', '515.123.4567', TO_DATE('17-06-1987', 'DD-MM-YYYY'), 'AD_PRES', 24000, NULL, NULL, 90);
+INSERT INTO MYSCHEMA.Employee VALUES (101, 'Neena', 'Kochhar', 'NKOCHHAR', '515.123.4568', TO_DATE('21-09-1989', 'DD-MM-YYYY'), 'AD_VP', 17000, NULL, 100, 90);
+INSERT INTO MYSCHEMA.Employee VALUES (102, 'Lex', 'De Haan', 'LDEHAAN', '515.123.4569', TO_DATE('13-01-1993', 'DD-MM-YYYY'), 'AD_VP', 17000, NULL, 100, 90);
+INSERT INTO MYSCHEMA.Employee VALUES (103, 'Alexander', 'Hunold', 'AHUNOLD', '590.423.4567', TO_DATE('03-01-1990', 'DD-MM-YYYY'), 'IT_PROG', 9000, NULL, 102, 60);
+INSERT INTO MYSCHEMA.Employee VALUES (104, 'Bruce', 'Ernst', 'BERNST', '590.423.4568', TO_DATE('21-05-1991', 'DD-MM-YYYY'), 'IT_PROG', 6000, NULL, 103, 60);
+INSERT INTO MYSCHEMA.Employee VALUES (107, 'Diana', 'Lorentz', 'DLORENTZ', '590.423.5567', TO_DATE('07-02-1999', 'DD-MM-YYYY'), 'IT_PROG', 4200, NULL, 103, 60);
+INSERT INTO MYSCHEMA.Employee VALUES (124, 'Kevin', 'Mourgos', 'KMOURGOS', '650.123.5234', TO_DATE('16-11-1999', 'DD-MM-YYYY'), 'ST_MAN', 5800, NULL, 100, 50);
+INSERT INTO MYSCHEMA.Employee VALUES (141, 'Trenna', 'Rajs', 'TRAJS', '650.121.8009', TO_DATE('17-10-1995', 'DD-MM-YYYY'), 'ST_CLERK', 3500, NULL, 124, 50);
+INSERT INTO MYSCHEMA.Employee VALUES (142, 'Curtis', 'Davies', 'CDAVIES', '650.121.2994', TO_DATE('29-01-1997', 'DD-MM-YYYY'), 'ST_CLERK', 3100, NULL, 124, 50);
+INSERT INTO MYSCHEMA.Employee VALUES (143, 'Randall', 'Matos', 'RMATOS', '650.121.2874', TO_DATE('15-03-1998', 'DD-MM-YYYY'), 'ST_CLERK', 2600, NULL, 124, 50);
+INSERT INTO MYSCHEMA.Employee VALUES (144, 'Peter', 'Vargas', 'PVARGAS', '650.121.2004', TO_DATE('09-07-1998', 'DD-MM-YYYY'), 'ST_CLERK', 2500, NULL, 124, 50);
+INSERT INTO MYSCHEMA.Employee VALUES (149, 'Eleni', 'Zlotkey', 'EZLOTKEY', '011.44.1344.429018', TO_DATE('29-01-2000', 'DD-MM-YYYY'), 'SA_MAN', 10500, 0.2, 100, 80);
+INSERT INTO MYSCHEMA.Employee VALUES (174, 'Ellen', 'Abel', 'EABEL', '011.44.1644.429267', TO_DATE('11-05-1996', 'DD-MM-YYYY'), 'SA_REP', 11000, 0.3, 149, 80);
+INSERT INTO MYSCHEMA.Employee VALUES (176, 'Jonathon', 'Taylor', 'JTAYLOR', '011.44.1644.429265', TO_DATE('24-03-1998', 'DD-MM-YYYY'), 'SA_REP', 8600, 0.2, 149, 80);
+INSERT INTO MYSCHEMA.Employee VALUES (178, 'Kimberely', 'Grant', 'KGRANT', '011.44.1644.429263', TO_DATE('24-05-1999', 'DD-MM-YYYY'), 'SA_REP', 7000, 0.15, 149, NULL);
+INSERT INTO MYSCHEMA.Employee VALUES (200, 'Jennifer', 'Whalen', 'JWHALEN', '515.123.4444', TO_DATE('17-09-1987', 'DD-MM-YYYY'), 'AD_ASST', 4400, NULL, 101, 10);
+INSERT INTO MYSCHEMA.Employee VALUES (201, 'Michael', 'Hartstein', 'MHARTSTE', '515.123.5555', TO_DATE('17-02-1996', 'DD-MM-YYYY'), 'MK_MAN', 13000, NULL, 100, 20);
+INSERT INTO MYSCHEMA.Employee VALUES (202, 'Pat', 'Fay', 'PFAY', '603.123.6666', TO_DATE('17-08-1997', 'DD-MM-YYYY'), 'MK_REP', 6000, NULL, 201, 20);
+INSERT INTO MYSCHEMA.Employee VALUES (205, 'Shelley', 'Higgins', 'SHIGGINS', '515.123.8080', TO_DATE('07-06-1994', 'DD-MM-YYYY'), 'AC_MGR', 12000, NULL, 101, 110);
+INSERT INTO MYSCHEMA.Employee VALUES (206, 'William', 'Gietz', 'WGIETZ', '515.123.8181', TO_DATE('07-06-1994', 'DD-MM-YYYY'), 'AC_ACCOUNT', 8300, NULL, 205, 110);
+INSERT INTO MYSCHEMA.Employee VALUES (207, 'TEST', 'TEST', 'TEST', '084.123.4567', TO_DATE('17-06-2018', 'DD-MM-YYYY'), 'IT_PROG', 24000, NULL, NULL, 60);
 
 insert into department values (10,'Administration',200,1700);
 insert into department values (20,'Marketing',201,1800);
@@ -104,11 +127,12 @@ insert into department values (90,'Executive',100,1700);
 insert into department values (110,'Accounting',205,1700);
 insert into department values (190,'Contracting',null,1700);
 
-INSERT INTO SYS.Job_Grades VALUES ('A', 1000, 2999);
-INSERT INTO SYS.Job_Grades VALUES ('B', 3000, 5999);
-INSERT INTO SYS.Job_Grades VALUES ('C', 6000, 9999);
-INSERT INTO SYS.Job_Grades VALUES ('D', 10000, 14999);
-INSERT INTO SYS.Job_Grades VALUES ('E', 15000, 24999);
-INSERT INTO SYS.Job_Grades VALUES ('F', 25000, 40000);
+INSERT INTO MYSCHEMA.Job_Grades VALUES ('A', 1000, 2999);
+INSERT INTO MYSCHEMA.Job_Grades VALUES ('B', 3000, 5999);
+INSERT INTO MYSCHEMA.Job_Grades VALUES ('C', 6000, 9999);
+INSERT INTO MYSCHEMA.Job_Grades VALUES ('D', 10000, 14999);
+INSERT INTO MYSCHEMA.Job_Grades VALUES ('E', 15000, 24999);
+INSERT INTO MYSCHEMA.Job_Grades VALUES ('F', 25000, 40000);
 
 COMMIT;
+--> ok
